7180e95bab3e814640c6d9e22a584c88e1ca6037
Petr Šimon <petr.simon@gmail.com>
2026-08-25 17:18:29 +0200

    fix(gui): render HRAI lesson modal

---
+++ packages/scratch-gui/test/unit/components/hrai-lessons-library.test.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import {fireEvent, screen} from '@testing-library/react';
+import configureStore from 'redux-mock-store';
+import {Provider} from 'react-redux';
+
+import HraiLessonsLibrary from '../../../src/components/hrai-lessons/hrai-lessons-library.jsx';
+import lessons from '../../../src/lib/hrai-lessons';
+import {renderWithIntl} from '../../helpers/intl-helpers.jsx';
+
+describe('HraiLessonsLibrary', () => {
+    test('opens a lesson detail from the lesson list', () => {
+        const store = configureStore()({locales: {isRtl: false}});
+        renderWithIntl(
+            <Provider store={store}>
+                <HraiLessonsLibrary
+                    lessons={lessons}
+                    onRequestClose={jest.fn()}
+                />
+            </Provider>
+        );
+
+        fireEvent.click(screen.getByRole('button', {name: /Bitva vojáků/}));
+
+        expect(screen.getByText('Připrav vojáky s mečem a lukem na bojišti.')).toBeTruthy();
+        expect(screen.getByText(/Startovní projekty a kontrola splnění přibudou/)).toBeTruthy();
+    });
+});
